c6cb65ecddf89bc3c4a644e6dfcbef7909ac2c82
Lekha Sathvik Devabathini <dlsathvik04@gmail.com>
2023-12-29 22:57:01 +0530

    initialized project

---
+++ src/main/java/com/gamesh/gamesh/GameshApplication.java
@@ -0,0 +1,13 @@
+package com.gamesh.gamesh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameshApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameshApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gamesh/gamesh/GameshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gamesh.gamesh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameshApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
